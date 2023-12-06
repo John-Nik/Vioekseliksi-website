bfa2c8faae094da296eedc3c14511ad739ece1bb
John N. <nikolaou.giannis@yahoo.com>
2023-12-07 00:26:48 +0200

    uploaded via vscode

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,21 @@
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+const { DateTime } = require('luxon');
+
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
     eleventyConfig.addPassthroughCopy('assets')
+
+    eleventyConfig.addFilter('postDate', (dateObj) => {
+		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+	});
+
     return {
         dir: {
             input: 'src',
-            includes: '_includes',
+            includes: 'includes',
             output: 'public',
+            data: 'data'
         },
     passthroughFileCopy: true,
     templateFormats: ['md', 'html', 'njk'],
